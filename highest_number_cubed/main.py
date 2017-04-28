95733bdd6b1ddc337d32e5e90dfd8e128d7b9e62
Yana <doodlemadness+py@gmail.com>
2017-04-28 01:50:05 +0000

    We did it reddit!

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,9 @@
 """This is the entry point of the program."""
 
 
-def highest_number_cubed(limit):
-    pass
+def highest_number_cubed(limit):    
+    number = 0    
+    while (number - 1) ** 3 < limit:        
+        number += 1        
+        if number ** 3 > limit:            
+            return number - 1
